7b318eeccea56f239f6a7e4d3fccc2a6babfd809
alex1115alex <alex1115alex@gmail.com>
2020-03-08 20:14:44 -0600

    asadas

--- index.js
+++ index.js
@@ -3,12 +3,20 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 8080;
 
+var MOTD = "Hello guys! This is the MOTD!";
+var messageArr = [MOTD, "yoloswag69"];
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  io.emit('chat message', "HELLO! THIS IS THE MOTD");
+  
+  for(var i = 0; i < messageArr.length; i++)
+  {
+    io.emit('chat message', messageArr[i]);
+  }
+
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
