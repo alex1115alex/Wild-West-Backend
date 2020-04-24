8442656091bad763e59fc7876809c094cdcf3461
jizh4220 <46586812+jizh4220@users.noreply.github.com>
2020-04-23 20:34:41 -0600

    Update register.js

--- js/register.js
+++ js/register.js
@@ -5,20 +5,6 @@ var usernameValid = false;
 var passwordValid = false;
 var passwordMatch = false;
 
-//This functions ensures that both the 'First Name' and 'Last Name' have some value
-function checkNames(){
-  if(document.getElementById('firstName').value.length == 0 || document.getElementById('lastName').value.length == 0){
-    document.getElementById('nameHelpBlock').innerHTML = "Please enter a first AND last name.";
-    nameValid = false;
-  }
-  else{
-    document.getElementById('nameHelpBlock').innerHTML = "";
-    document.getElementById('firstName').classList.add('is-valid');
-    document.getElementById('lastName').classList.add('is-valid')
-    nameValid = true;
-  }
-}
-
 //This function uses a regular expression to ensure the email is valid
 function checkEmail(){
   var regexEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
