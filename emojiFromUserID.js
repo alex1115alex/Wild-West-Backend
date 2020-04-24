2c8fcff523c4469791de227c3b7a70b7fbdd36fd
Alex Israelov <alex1115alex@gmail.com>
2020-04-24 15:30:07 -0500

    Fixed some bugs within the emoji hasing system

--- emojiFromUserID.js
+++ emojiFromUserID.js
@@ -3,18 +3,23 @@ module.exports = {
     //helper function
     stringToEmojiNumber: function stringToEmojiNumber(len, s) {
         var totalNumEmojis = len;
+        console.log("Total number of emojis: " + totalNumEmojis);
 
         var h = 0, l = s.length, i = 0;
         if (l > 0)
             while (i < l)
                 h = (h << 5) - h + s.charCodeAt(i++) | 0;
 
-        //my retarded algorithm
+        //my retarded algorithm: I call it Alex's emoji machine
+
+        //if the hash number is negative, make it not negative
+        if(h < 0){
+            h *= -1;
+        }
+
+        //if the number is > the totalNumEmojis then reduce it. Probably not super efficient.
         while (h > totalNumEmojis) {
             h = h % totalNumEmojis;
-        }
-        if (h == 0) {
-            h = 1;
         }
 
         return h;
@@ -24,7 +29,8 @@ module.exports = {
     //PARAMETERS: An array of emojis and a string
     //RETURNS: An emoji hashed from the string
     stringToEmoji: function stringToEmoji(emojiArr, s) {
-        var num = this.stringToEmojiNumber(emojiArr.length, s);
+        var len = emojiArr.length;
+        var num = this.stringToEmojiNumber(len, s);
         return emojiArr[num];
     }
 };
